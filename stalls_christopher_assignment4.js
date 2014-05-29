38c4f84f24c7c6adb44529490f143313bf4e56d8
Christopher Stalls <christopherstalls@christophersmbp.att.net>
2014-05-29 09:57:16 -0500

    first function

--- stalls_christopher_assignment4.js
+++ stalls_christopher_assignment4.js
@@ -1,1 +1,17 @@
-alert("JavaScript works!");
+//alert("JavaScript works!");
+
+//Christopher Stalls
+//SDI 1405
+//Project 4
+//Functions
+
+//Format number to use 2 decimal places
+function convertMoney (itemTotal, tax)
+{
+	var total;
+	total = itemTotal * tax;
+	
+	return total.tofixed(2);
+}
+
+//
